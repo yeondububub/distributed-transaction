c7b7e9b8ce75544b03871f855df00db39e2f5f79
ydbb <yeondububub@gmail.com>
2026-05-28 17:24:37 +0900

    tcc 모듈 생성

---
+++ tcc-product/src/test/java/com/example/tccproduct/TccProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tccproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
